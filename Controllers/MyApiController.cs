e21ad96cd4eaad30d1a0a80bf2238c101b92f5af
harinaveen01 <harinaveen01@gmail.com>
2019-11-12 11:06:00 +0530

    none

---
+++ Controllers/MyApiController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Http;
+using System.Collections.Generic;
+using CustomMvc.Models.CustomMvcClass;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+namespace CustomMvc.Controllers
+{
+    public class MyApiController : ApiController
+    {
+
+        //public IList<FormTable> Get()
+        //{
+        //    FormTable ObjTable = new FormTable();
+        //    IList<FormTable> ListOfFormTable = ObjTable.TableList();
+        //    return ListOfFormTable;
+        //}
+        public JObject Get()
+        {
+            JObject Jobj = new JObject();
+            FormTable ObjTable = new FormTable();
+            List<FormTable> ListOfFormTable = ObjTable.TableList();
+            Jobj["User"] = JArray.FromObject(ListOfFormTable.Select(x => new { x.FormName ,x.FormType,x.FormJavaScript})) ;
+
+            return Jobj;
+        }
+      
+    }
+}
--- Global.asax.cs
+++ Global.asax.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Optimization;
+using System.Web.Http;
 using System.Web.Routing;
+using System.Web.Optimization;
+
 
 namespace CustomMvc
 {
@@ -13,12 +15,12 @@ namespace CustomMvc
         protected void Application_Start()
         {
 
-
-
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
         }
     }
 }
